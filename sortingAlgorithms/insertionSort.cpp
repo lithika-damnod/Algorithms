1e56cc885210e3343f2a71da2345b93223263c29
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-10-27 20:58:26 +0530

    working on insertionSort

---
+++ sortingAlgorithms/insertionSort.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include <vector> 
+#include <string> 
+using namespace std; 
+
+//insertion sort <int>
+vector<int> insertionSort(vector<int> arr){
+	for(int i=1; i<arr.size(); i++){
+		int index = i; 
+		if(arr[i-1] > arr[i]){
+			while()
+		}
+	}
+}
+
+int main(){
+	vector<int>para = {1, 4, 2, 10, 2, 3, 5};
+	insertionSort(para); 
+	return 0; 
+}
