7753f53d4d3b57b01e39eeec6b9fcaefe91b58ec
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-10-28 18:27:00 +0530

    done with insertionSort ASC

--- sortingAlgorithms/insertionSort.cpp
+++ sortingAlgorithms/insertionSort.cpp
@@ -3,21 +3,16 @@
 #include <string> 
 using namespace std; 
 
-//insertion sort <int>
+//insertion sort ASCENDING 
 vector<int> insertionSort(vector<int> arr){
-	for(int i=1; i<arr.size(); i++){
-		int index = i; 
-		if(arr[i-1] > arr[i]){
-			int num = arr[i]; 
-			while(arr[index-1] > arr[i]){
-				//working on while loop loop
-			}	
+	for(int j=1; j<arr.size(); j++){
+		int key = arr[j]; 
+		int i = j-1; 
+		while(i > 0 && arr[i] > key){
+			arr[i + 1] = arr[i]; 
+			i = i - 1; 
 		}
+		arr[i+1] = key; 
 	}
-}
-
-int main(){
-	vector<int>para = {1, 4, 2, 10, 2, 3, 5};
-	insertionSort(para); 
-	return 0; 
+	return arr; 
 }
