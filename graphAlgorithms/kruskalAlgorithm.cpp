aae1e4d2fe93ece04f0f2b85ca71be5eaaf840c2
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-10-31 16:47:50 +0530

    removed all the couts

--- graphAlgorithms/kruskalAlgorithm.cpp
+++ graphAlgorithms/kruskalAlgorithm.cpp
@@ -6,18 +6,10 @@ using namespace std;
 
 //get metrices
 void getMetrices(){
-	bool similarFound = false; 
-	//while loop not working properly : TODO
-	//while doesn't loop when there's similar vertex letters found	
-	while(!similarFound){
-		cout << "number of vertices: "; 
-		int nV; cin >> nV; 
-		cout << "enter vertex letter" << endl; 
-		vector<char> charArr; 
-		for(int i=0; i<nV; i++){
-			cout << "vertex " << i + 1 << ": "; 
-			char vChar; cin >> vChar; 	
-			charArr.push_back(vChar); 
+	int nV; cin >> nV; 
+	for(int i=0; i<nV; i++){
+		for(int j=0; j<nV; j++){
+			int tmpChar; cin >> tmpChar;
 		}
 	}
 }
