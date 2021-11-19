d4013392100213fe0c3a75adb08ec25a523183fe
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-11-19 17:59:03 +0530

    finished bubbleSort.h for Integers

--- sortingAlgorithms/bubbleSort.h
+++ sortingAlgorithms/bubbleSort.h
@@ -6,15 +6,21 @@ using namespace std;
 
 //bubble sort algorithm for integers
 vector<int> bubbleSort(vector<int> arr){
-    //developing for first pass 
-    for(int j=1; j<arr.size(); j++){
-        int i=1-1; 
-        if(arr[i] > arr[j]){
-            int tmp = arr[j]; 
-            arr[j]  = arr[i]; 
-            arr[i] = tmp; 
+    int nR = 0; 
+    int nM = 0; 
+    while((nR == 0 && nM == 0) || nM != 0){
+        nM = 0; 
+        nR = 0; 
+        for(int j=1; j<arr.size(); j++){
+            int i=j-1; 
+            if(arr[i] > arr[j]){
+                nM++; 
+                int tmp = arr[j]; 
+                arr[j]  = arr[i]; 
+                arr[i] = tmp; 
+            }
         }
+        nR++; 
     }
-    
     return arr; 
 }
