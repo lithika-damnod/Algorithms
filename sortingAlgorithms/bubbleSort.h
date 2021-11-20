b178c2afd4dbbfc30152376a8df494019a40afde
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-11-20 09:14:41 +0530

    added desc sort for the bubbleSort

--- sortingAlgorithms/bubbleSort.h
+++ sortingAlgorithms/bubbleSort.h
@@ -12,7 +12,8 @@ using namespace std;
 */
 
 // bubble sort algorithm for integers
-vector<int> bubbleSort(vector<int> arr, int &mode){
+vector<int> bubbleSort(vector<int> arr, int mode){
+    // ASCENDING
     if(mode == 0){    
         int nR = 0; 
         int nM = 0;     
@@ -22,6 +23,25 @@ vector<int> bubbleSort(vector<int> arr, int &mode){
             for(int j=1; j<arr.size(); j++){
                 int i=j-1; 
                 if(arr[i] > arr[j]){
+                    nM++; 
+                    int tmp = arr[j]; 
+                    arr[j]  = arr[i]; 
+                    arr[i] = tmp; 
+                }
+            }
+            nR++;   
+        }
+    }
+    // DESCENDING
+    else if(mode == 1){    
+        int nR = 0; 
+        int nM = 0;     
+        while((nR == 0 && nM == 0) || nM != 0){
+            nM = 0; 
+            nR = 0; 
+            for(int j=1; j<arr.size(); j++){
+                int i=j-1; 
+                if(arr[i] < arr[j]){
                     nM++; 
                     int tmp = arr[j]; 
                     arr[j]  = arr[i]; 
