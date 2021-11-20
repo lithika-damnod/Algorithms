6d90e911ef74f9e87db16176ef13b48778952246
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-11-20 20:32:41 +0530

    modified: quickSort.h

---
+++ sortingAlgorithms/quickSort.h
@@ -0,0 +1,50 @@
+#pragma once 
+
+#include <iostream> 
+#include <vector>
+using namespace std; 
+
+
+/*
+    quickSort(arr, mode)
+    pass parameter 0 for ASCENDING order
+    pass parameter 1 for DESCENDING order
+*/
+
+// quick sort algorithm for integers
+vector<int> quickSort(vector<int> arr, int mode){
+    //  ASCENDING
+    if(mode == 0){
+        int nPivots = 0;  
+        while(nPivots != arr.size()){
+            int baseIndex = (arr.size() + 1) / 2;  
+            int basePivot = arr[baseIndex];
+            vector<int> arrClone = arr; 
+            for(int i=1; i<baseIndex; i++){
+                int j = i - 1; 
+                if(arr[j] > arr[i]){
+                    int tmp = arr[j]; 
+                    arr[j] = arr[i]; 
+                    arr[i] = tmp; 
+                }
+            }
+            for(int i=arr.size(); i > baseIndex; i--){
+                int j = i - 1; 
+                if(arr[j] > arr[i]){
+                    if(arr[j] == baseIndex ){
+                        break; 
+                    }
+                    int tmp = arr[j]; 
+                    arr[j] = arr[i]; 
+                    arr[i] = tmp; 
+                }
+            }
+        }
+        return arr; 
+    }
+    // DESCENDING
+    else if(mode == 1){
+        return arr; 
+    }
+    return arr;     
+}
