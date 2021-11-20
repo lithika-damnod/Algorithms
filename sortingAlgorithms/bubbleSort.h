825671ba77587f0fac91e900368a77f7cbfc53b8
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-11-20 08:06:33 +0530

    added some comments with modes

--- sortingAlgorithms/bubbleSort.h
+++ sortingAlgorithms/bubbleSort.h
@@ -4,23 +4,32 @@
 #include <vector>
 using namespace std; 
 
-//bubble sort algorithm for integers
-vector<int> bubbleSort(vector<int> arr){
-    int nR = 0; 
-    int nM = 0; 
-    while((nR == 0 && nM == 0) || nM != 0){
-        nM = 0; 
-        nR = 0; 
-        for(int j=1; j<arr.size(); j++){
-            int i=j-1; 
-            if(arr[i] > arr[j]){
-                nM++; 
-                int tmp = arr[j]; 
-                arr[j]  = arr[i]; 
-                arr[i] = tmp; 
+
+/*
+    bubbleSort(arr, mode)
+    pass parameter 0 for ASCENDING order
+    pass parameter 1 for DESCENDING order
+*/
+
+// bubble sort algorithm for integers
+vector<int> bubbleSort(vector<int> arr, int &mode){
+    if(mode == 0){    
+        int nR = 0; 
+        int nM = 0; 
+        while((nR == 0 && nM == 0) || nM != 0){
+            nM = 0; 
+            nR = 0; 
+            for(int j=1; j<arr.size(); j++){
+                int i=j-1; 
+                if(arr[i] > arr[j]){
+                    nM++; 
+                    int tmp = arr[j]; 
+                    arr[j]  = arr[i]; 
+                    arr[i] = tmp; 
+                }
             }
+            nR++; 
         }
-        nR++; 
     }
     return arr; 
 }   
