b93d1fb34d9a429c68872e5691c84d43f38c29b2
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-11-24 21:19:01 +0530

    reimplementing firstBin.h from the beginning

--- binPackingAlgorithms/firstBin.h
+++ binPackingAlgorithms/firstBin.h
@@ -29,21 +29,7 @@ vector<vector<int>> firstBin(vector<int> arr, int size){
     vector<vector<int>> retArr; 
     
     for(int i=0; i<arr.size(); i++){
-        int indexUp = i; 
-        int sum = 0;  
-        vector<int> push; 
         
-        while( (sum < size)  && (indexUp < arr.size())){
-            sum += arr[indexUp];
-            push.push_back(arr[indexUp]); 
-            indexUp++;  
-        }
-
-        push.pop_back(); 
-        retArr.push_back(push);
-        i = indexUp - 1; 
-        push.clear();   
-        sum = 0; 
     }
 
     return retArr; 
