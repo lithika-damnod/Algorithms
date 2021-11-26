76bf4ea11ad476d41a107342b90c2f2c1d59ef7c
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-11-26 16:42:40 +0530

    edited comments in firstBinDecreasing.h

--- binPackingAlgorithms/firstBinDecreasing.h
+++ binPackingAlgorithms/firstBinDecreasing.h
@@ -3,13 +3,22 @@
 #include <iostream> 
 #include <vector> 
 #include "firstBin.h" // for firstBin()
-#include "../sortingAlgorithms/bubbleSort.h" // for sorting into DESCENDING ORDER
+#include "../sortingAlgorithms/bubbleSort.h" // for sorting into descending order
 
-
+/*
+    firstBinDecreasing(arr, size)
+    arr: vector<vector<int>> (two dimensional vector)
+    size: int
+    returns: vector<vector<int>> (two dimensional vector)
+*/
 
 vector<vector<int>> firstBinDecreasing(vector<int> arr, int size){
     
-    vector<int> descArr = bubbleSort(arr, 1); 
+    // sort the array into descending order
+    vector<int> descArr = bubbleSort(arr, 1);
+
+    // applying firstBin algorithm "firstBin()"
     vector<vector<int>> output = firstBin(descArr, size); 
+    
     return output; 
 }
