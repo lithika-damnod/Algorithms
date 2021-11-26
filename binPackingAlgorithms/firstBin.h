8536f2d1f1b19e2cccc557ac4edd7a838d08f528
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-11-26 15:57:13 +0530

    rewriting firstBin.h cause it's not working as I expected

--- binPackingAlgorithms/firstBin.h
+++ binPackingAlgorithms/firstBin.h
@@ -25,28 +25,6 @@ int lowerBound(vector<int> arr, int size){
 }
 
 vector<vector<int>> firstBin(vector<int> arr, int size){
-    
-    vector<vector<int>> retArr; 
-    int sum = 0; 
-    int i = 0; 
-    vector<int> push; 
-    while(i < arr.size()){
-        while(sum < size){
-            sum += arr[i];
-            if(sum > size){
-                break; 
-            }
-            push.push_back(arr[i]);  
-            cout << "pushing: " << arr[i] << endl; 
-            i++; 
-            cout << "i: " << i << endl;
-            cout << "sum: " << sum << endl;  
-        }
-        retArr.push_back(push); 
-        push.clear(); 
-        cout << "----------" << endl; 
-        sum = 0; 
-    }
 
-    return retArr; 
+    return; 
 }
