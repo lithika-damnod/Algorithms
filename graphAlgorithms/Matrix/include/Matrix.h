942c818fb06f1a6d678d7844c885166cdfc571b0
lithika-damnod <l.d.b.dasanayaka@gmail.com>
2021-12-13 15:13:37 +0530

    created two constructors and print function for Matrix class

--- graphAlgorithms/Matrix/include/Matrix.h
+++ graphAlgorithms/Matrix/include/Matrix.h
@@ -1,16 +1,21 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
-
+#include <iostream>
+#include <vector>
+using namespace std;
 class Matrix
 {
     public:
         Matrix();
+        Matrix(int n);
         virtual ~Matrix();
-
+        void print();
     protected:
 
     private:
+        int nV = 0;
+        vector<vector<int>> data;
 };
 
 #endif // MATRIX_H
